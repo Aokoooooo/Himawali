d2582a75d88fb495f75e01195979606036b41990
Aoko <hejunqin233@gmail.com>
2019-07-11 17:45:22 +0800

    (feature): 增加对环境是否支持Proxy功能的检查

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+if (typeof Proxy === "undefined") {
+    throw new Error("himawari-store requires Proxy object which your environment doesn't support");
+}
+
 import { createStore, IStoreProps } from "./createStore";
 import { useStore } from "./hooks";
 import store, { IStore, IStores } from "./stores";
