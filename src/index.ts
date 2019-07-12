7ba0ab35b8bfb5994ab301b461e68dd06cb4af65
Aoko <hejunqin233@gmail.com>
2019-07-12 22:14:42 +0800

    (feature): 增加对connect方法的导出

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ if (typeof Proxy === "undefined") {
     throw new Error("himawari-store requires Proxy object which your environment doesn't support");
 }
 
+import { connect } from "./connect";
 import { createStore, IStoreProps } from "./createStore";
 import { useStore } from "./hooks";
 import store, { IStore, Store } from "./stores";
 
-export { store, useStore, createStore, IStore, IStoreProps, Store };
+export { store, useStore, createStore, IStore, IStoreProps, Store, connect };
